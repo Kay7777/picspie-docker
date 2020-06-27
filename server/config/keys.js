97d9b2c4124c0cec97d4eae5eb31386fccfc8236
kay7777 <kay7song@gmail.com>
2020-06-26 22:42:11 -0600

    asd

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,7 +1,7 @@
 const os = require("os");
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./prod");
-} else if (os.environ.get("TRAVIS") == "true") {
+} else if (process.env.NODE_ENV === "ci" || process.env.NODE_ENV === "CI") {
   module.exports = require("./ci");
 } else {
   module.exports = require("./dev");
