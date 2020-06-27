ae685f8a0d000843c8bec6415035150fde8cece4
kay7777 <kay7song@gmail.com>
2020-06-26 22:54:35 -0600

    asd

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./prod");
-} else if (process.env.NODE_ENV === "ci" || process.env.NODE_ENV === "CI") {
+} else if (process.env.NODE_ENV === "TEST") {
   module.exports = require("./ci");
 } else {
   console.log("current enviroment is :", process.env.NODE_ENV);
